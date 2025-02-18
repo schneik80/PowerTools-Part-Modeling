36c8dfc4d39b2646b4aff80c5b8c1daf20b188b0
Kevin Schneider <schneik80@gmail.com>
2025-02-18 10:26:07 -0800

    merge

--- config.py
+++ config.py
@@ -7,11 +7,8 @@ DEBUG = True
 ADDIN_NAME = os.path.basename(os.path.dirname(__file__))
 COMPANY_NAME = "IMA LLC"
 
-<<<<<<< Updated upstream
 COMPANY_HUB = ""
 
-=======
->>>>>>> Stashed changes
 
 design_workspace = "FusionSolidEnvironment"
 tools_tab_id = "ToolsTab"
@@ -20,7 +17,3 @@ my_tab_name = "Power Tools"
 my_panel_id = f"{ADDIN_NAME}_panel_2"
 my_panel_name = "Power Tools"
 my_panel_after = ""
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
